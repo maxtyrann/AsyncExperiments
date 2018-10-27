14a49050c80c33c32f6272ed4693032a6f4edb57
murat <murat@DESKTOP-043C69S>
2018-10-28 00:06:28 +0300

    Added continuation scenarios

--- AsyncExperiments.Threaded/Program.cs
+++ AsyncExperiments.Threaded/Program.cs
@@ -21,6 +21,7 @@ namespace AsyncExperiments.Threaded
             Console.WriteLine("3. EapTest - Same operation with Event-based Asynchronous Pattern");
             Console.WriteLine("4. AsyncTestWithFaults - Some error handling experiments");
             Console.WriteLine("5. AsyncTestWaitWithFault - Some error handling experiments take two, includes one scenario (commented) which will crash the process");
+            Console.WriteLine("6. ContinuationsTest - Continuation scenarios");
 
             var result = Console.ReadLine();
 
@@ -40,6 +41,9 @@ namespace AsyncExperiments.Threaded
                     break;
                 case "5":
                     AsyncTestWaitWithFault();
+                    break;
+                case "6":
+                    ContinuationsTest();
                     break;
                 default:
                     MultiThreadedTest();
@@ -101,15 +105,28 @@ namespace AsyncExperiments.Threaded
             var webClient = new WebClient();
             Console.WriteLine("Starting work");
 
-            // this doesn't crash the process
+            //todo:Murat Refactor this scenario into another method
             webClient.OpenWriteTaskAsync(new Uri("https://localhost:20202")).ContinueWith(t => {
-                Console.WriteLine(t.Result);
+                Console.WriteLine("Abandoned task, nothing happens. I was expecting to hit UnobservedTaskException");
             });
+
+            // this doesn't crash the process
+            //webClient.OpenWriteTaskAsync(new Uri("https://localhost:20202")).ContinueWith(t => {
+            //    Console.WriteLine(t.Result);
+            //});
 
             // but this does
             // webClient.OpenWriteTaskAsync(new Uri("https://localhost:20202")).Wait();
 
             Console.WriteLine("If we use Wait, process will crash");
+            Console.ReadLine();
+
+            // For UnobservedTaskException to work:
+            // 1. TPL depends on the Garbage Collector's finalization mechanism to detect unobserved exceptions. This code doesn't do anything so unless we force one
+            // we'll never going to see garbage collection (GC.Collect)
+            // 2. Program needs to be built in Release mode
+            // 3. Start without debugging (Ctrl+F5)
+            GC.Collect();
         }
 
         /// <summary>
@@ -180,6 +197,33 @@ namespace AsyncExperiments.Threaded
 
                 // Wait, WaitForAll, WaitForAny methods also throw an exception if task is faulted
             });
+
+            Console.WriteLine("Continuing on main thread");
+        }
+
+        private static void ContinuationsTest()
+        {
+            var webClient = new WebClient();
+            Console.WriteLine("Starting work");
+
+            // This time we're using DownloadStringTaskAsync method and this time it's async
+            Task<string> getTask = webClient.DownloadStringTaskAsync("https://warfarehistorynetwork.com/daily/military-history/trafalgar-in-reverse-the-battle-of-jutland/");
+
+            Console.WriteLine("Setting up continuation");
+            getTask.ContinueWith(t =>
+            {
+                // only run if there's no fault
+                Console.WriteLine(t.Result);
+            }, TaskContinuationOptions.OnlyOnRanToCompletion);
+
+            //Task<string> getTask2 = webClient.DownloadStringTaskAsync("https://localhost:20202");
+
+            //Console.WriteLine("Setting up continuation");
+            //getTask2.ContinueWith(t =>
+            //{
+            //    // only run if task is faulted
+            //    Console.WriteLine(t.Exception);
+            //}, TaskContinuationOptions.OnlyOnFaulted);
 
             Console.WriteLine("Continuing on main thread");
         }
