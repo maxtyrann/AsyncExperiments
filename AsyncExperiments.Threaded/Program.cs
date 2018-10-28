bb86b64a9c80af7b6bbcdba208b747b932c8ea4b
maxtyrann <muratkebabci@gmail.com>
2018-10-28 14:32:47 +0300

    Further improvements, added a return to menu and AutoResetEvent for blocking the main thread

--- AsyncExperiments.Threaded/Program.cs
+++ AsyncExperiments.Threaded/Program.cs
@@ -25,6 +25,8 @@ namespace AsyncExperiments.TapAndEapSamples
         private const int MINIMIZE = 6;
         private const int RESTORE = 9;
 
+        private static AutoResetEvent autoResetEvent = new AutoResetEvent(false);
+
         static void Main(string[] args)
         {
             ShowWindow(ThisConsole, MAXIMIZE);
@@ -32,7 +34,14 @@ namespace AsyncExperiments.TapAndEapSamples
             // works with conditions
             TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;
 
-            Console.WriteLine("SELECT AN OPERATION");
+            Menu();
+
+            autoResetEvent.WaitOne();
+        }
+
+        private static void Menu()
+        {
+            Console.WriteLine("SELECT AN OPERATION:");
             Console.WriteLine("");
             Console.WriteLine("1. UsingTaskFactory - Uses Task.Factory.StartNew and continuation with success");
             Console.WriteLine("2. UsingTaskDirectly - Same operation but this time it uses DownloadStringTaskAsync and continuation with success");
@@ -43,6 +52,10 @@ namespace AsyncExperiments.TapAndEapSamples
             Console.WriteLine("7. UsingTaskDirectlySimulatingFaults_AbandoningTaskException - In this scenario, task exception is abandoned. If you follow given instructions in the code this unobserved exception will be handled by UnobservedTaskException event");
             Console.WriteLine("8. UsingContinuations_OnlyOnSuccess - Continuation scenario, works only antecedent task is successful");
             Console.WriteLine("9. UsingContinuations_OnlyOnFault - Continuation scenario, works only antecedent task is faulted");
+            Console.WriteLine("10. UsingComposition_WhenAll_AllSuccess - Composition scenario, all sub tasks are successful");
+            Console.WriteLine("11. UsingComposition_WhenAll_OneFail - Composition scenario, one of sub tasks will fail");
+            Console.WriteLine("12. UsingComposition_WhenAll_AllFail - Composition scenario, both sub tasks will fail");
+            Console.WriteLine("13. UsingComposition_ContinueWhenAll_OneFail - Composition scenario with ContinueWhenAll, one sub tasks will fail but we can still track the other");
             Console.WriteLine("");
 
             var result = Console.ReadLine();
@@ -76,12 +89,43 @@ namespace AsyncExperiments.TapAndEapSamples
                 case "9":
                     UsingContinuations_OnlyOnFault();
                     break;
+                case "10":
+                    UsingComposition_WhenAll_AllSuccess();
+                    break;
+                case "11":
+                    UsingComposition_WhenAll_OneFail();
+                    break;
+                case "12":
+                    UsingComposition_WhenAll_AllFail();
+                    break;
+                case "13":
+                    UsingComposition_ContinueWhenAll_OneFail();
+                    break;
                 default:
                     UsingTaskFactory();
                     break;
             }
+        }
 
-            Console.ReadLine();
+        private static void ReturnToMenu()
+        {
+            Console.WriteLine("Do you want to return to Menu ? (Y/N)");
+            var menuAnswer = Console.ReadLine();
+            switch (menuAnswer)
+            {
+                case "Y":
+                    Console.Clear();
+                    Menu();
+                    break;
+                case "N":
+                    Console.WriteLine("Process will exit after Enter");
+                    autoResetEvent.Set();
+                    break;
+                default:
+                    Console.WriteLine("Process will exit after Enter");
+                    autoResetEvent.Set();
+                    break;
+            }
         }
 
         private static void TaskScheduler_UnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
@@ -109,6 +153,8 @@ namespace AsyncExperiments.TapAndEapSamples
             getTask.ContinueWith(t =>
             {
                 Console.WriteLine(t.Result);
+
+                ReturnToMenu();
             });
 
             Console.WriteLine("Continuing on main thread");
@@ -126,6 +172,8 @@ namespace AsyncExperiments.TapAndEapSamples
             getTask.ContinueWith(t =>
             {
                 Console.WriteLine(t.Result);
+
+                ReturnToMenu();
             });
 
             Console.WriteLine("Continuing on main thread");
@@ -150,6 +198,8 @@ namespace AsyncExperiments.TapAndEapSamples
         {
             Console.WriteLine("WebClient_DownloadStringCompleted is called as soon as we get a result");
             Console.WriteLine(e.Result);
+
+            ReturnToMenu();
         }
 
         private static void UsingTaskDirectlySimulatingFaults()
@@ -198,6 +248,8 @@ namespace AsyncExperiments.TapAndEapSamples
                 Console.WriteLine(t.Result);
 
                 // Wait, WaitForAll, WaitForAny methods also throw an exception if task is faulted
+
+                ReturnToMenu();
             });
 
             Console.WriteLine("Continuing on main thread");
@@ -212,6 +264,8 @@ namespace AsyncExperiments.TapAndEapSamples
             webClient.OpenWriteTaskAsync(new Uri("https://localhost:20202")).ContinueWith(t =>
             {
                 Console.WriteLine(t.Result);
+
+                ReturnToMenu();
             });
 
             Console.WriteLine("If we use Wait, process will crash");
@@ -244,6 +298,8 @@ namespace AsyncExperiments.TapAndEapSamples
                 Console.WriteLine("This task will throw an exception but since there's no call to Result or Wait, exception is unobserved");
                 Console.WriteLine("If you follow given instructions, this unobserved exception will be handled by TaskScheduler's UnobservedTaskException event");
                 Console.WriteLine("If you don't, nothing will happen");
+
+                ReturnToMenu();
             });
 
             Console.WriteLine("Continuing on main thread");
@@ -265,6 +321,8 @@ namespace AsyncExperiments.TapAndEapSamples
             {
                 // only run if there's no fault
                 Console.WriteLine(t.Result);
+
+                ReturnToMenu();
             }, TaskContinuationOptions.OnlyOnRanToCompletion);
 
             Console.WriteLine("Continuing on main thread");
@@ -282,7 +340,148 @@ namespace AsyncExperiments.TapAndEapSamples
             {
                 // only run if task is faulted
                 Console.WriteLine(t.Exception);
+
+                ReturnToMenu();
             }, TaskContinuationOptions.OnlyOnFaulted);
+
+            Console.WriteLine("Continuing on main thread");
+        }
+
+        // Task.WhenAll is used for task composition. Composed task is considered as successful only if all individual tasks are successful
+        // In this scenario, all tasks are successful so composed task is also successful therefore continuation will hit Result
+        private static void UsingComposition_WhenAll_AllSuccess()
+        {
+            var webClient1 = new WebClient();
+            var webClient2 = new WebClient();
+            Console.WriteLine("Starting work");
+
+            // This time we're using DownloadStringTaskAsync method and this time it's async
+            Task<string> getSubTask1 = webClient1.DownloadStringTaskAsync("https://warfarehistorynetwork.com/daily/military-history/trafalgar-in-reverse-the-battle-of-jutland/");
+            Task<string> getSubTask2 = webClient2.DownloadStringTaskAsync("https://warfarehistorynetwork.com/daily/wwii/field-marshal-erwin-rommel-the-battle-of-kasserine-pass/");
+
+            Task<string[]> getTask = Task.WhenAll(getSubTask1, getSubTask2);
+
+            Console.WriteLine("Setting up continuation");
+            getTask.ContinueWith(t =>
+            {
+                Console.WriteLine("Completed");
+                if (t.IsFaulted)
+                {
+                    // 
+                    Console.WriteLine(t.Exception);
+                }
+                else {
+                    // t.Result will be a string[] so ToString wont help
+                    Console.WriteLine(string.Join(Environment.NewLine, t.Result));
+
+                    ReturnToMenu();
+                }
+            });
+
+            Console.WriteLine("Continuing on main thread");
+        }
+
+        // This time getSubTask2 will fail, this failure causes composed task to fail
+        // There's no partial success or partial failure with WhenAll, if one individual task fails, composed task always fails
+        private static void UsingComposition_WhenAll_OneFail()
+        {
+            var webClient1 = new WebClient();
+            var webClient2 = new WebClient();
+            Console.WriteLine("Starting work");
+
+            // This time we're using DownloadStringTaskAsync method and this time it's async
+            Task<string> getSubTask1 = webClient1.DownloadStringTaskAsync("https://warfarehistorynetwork.com/daily/military-history/trafalgar-in-reverse-the-battle-of-jutland/");
+            Task<string> getSubTask2 = webClient2.DownloadStringTaskAsync("https://localhost:20202");
+
+            Task<string[]> getTask = Task.WhenAll(getSubTask1, getSubTask2);
+
+            Console.WriteLine("Setting up continuation");
+            getTask.ContinueWith(t =>
+            {
+                Console.WriteLine("Completed");
+                if (t.IsFaulted)
+                {
+                    Console.WriteLine($"Inner exceptions count: {t.Exception.InnerExceptions.Count}");
+                    Console.WriteLine(t.Exception);
+
+                    ReturnToMenu();
+                }
+                else
+                {
+                    Console.WriteLine(t.Result);
+                }
+            });
+
+            Console.WriteLine("Continuing on main thread");
+        }
+
+        // This time both sub tasks will fail, composed task will also fail
+        // Only difference with UsingComposition_WhenAll_OneFail is composed task's Exception property
+        // With both sub tasks fail, composed task's Exception property will include both of sub tasks Exceptions inside its InnerExceptions property
+        private static void UsingComposition_WhenAll_AllFail()
+        {
+            var webClient1 = new WebClient();
+            var webClient2 = new WebClient();
+            Console.WriteLine("Starting work");
+
+            // This time we're using DownloadStringTaskAsync method and this time it's async
+            Task<string> getSubTask1 = webClient1.DownloadStringTaskAsync("https://localhost:20203");
+            Task<string> getSubTask2 = webClient2.DownloadStringTaskAsync("https://localhost:20202");
+
+            Task<string[]> getTask = Task.WhenAll(getSubTask1, getSubTask2);
+
+            Console.WriteLine("Setting up continuation");
+            getTask.ContinueWith(t =>
+            {
+                Console.WriteLine("Completed");
+                if (t.IsFaulted)
+                {
+                    Console.WriteLine($"Inner exceptions count: {t.Exception.InnerExceptions.Count}");
+                    Console.WriteLine(t.Exception);
+
+                    ReturnToMenu();
+                }
+                else
+                {
+                    Console.WriteLine(t.Result);
+                }
+            });
+
+            Console.WriteLine("Continuing on main thread");
+        }
+
+        // If we need to track each one of sub tasks, WhenAll is meaningless because it's a one-for-all mechanism
+        private static void UsingComposition_ContinueWhenAll_OneFail()
+        {
+            var webClient1 = new WebClient();
+            var webClient2 = new WebClient();
+            Console.WriteLine("Starting work");
+
+            // This time we're using DownloadStringTaskAsync method and this time it's async
+            Task<string> getSubTask1 = webClient1.DownloadStringTaskAsync("https://warfarehistorynetwork.com/daily/military-history/trafalgar-in-reverse-the-battle-of-jutland/");
+            Task<string> getSubTask2 = webClient2.DownloadStringTaskAsync("https://localhost:20202");
+
+            Console.WriteLine($"Setting up continuation for tasks {getSubTask1.Id} and {getSubTask2.Id}");
+
+            Task.Factory.ContinueWhenAll(new[] { getSubTask1, getSubTask2 }, 
+                (Task<string>[] tasks) => {
+                    foreach (var t in tasks)
+                    {
+                        Console.WriteLine($"Completed task: {t.Id}");
+                        if (t.IsFaulted)
+                        {
+                            Console.WriteLine($"Inner exceptions count for task {t.Id}: {t.Exception.InnerExceptions.Count}");
+                            Console.WriteLine(t.Exception);
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Task {t.Id} is successful");
+                            Console.WriteLine(t.Result);
+                        }
+                    }
+
+                    ReturnToMenu();
+                });
 
             Console.WriteLine("Continuing on main thread");
         }
