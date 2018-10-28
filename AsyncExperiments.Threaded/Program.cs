622297581bcac5f1597d7564a0db063384423c82
maxtyrann <muratkebabci@gmail.com>
2018-10-28 11:46:19 +0300

    Refactored a bit more, no commenting or uncommenting needed right now

--- AsyncExperiments.Threaded/Program.cs
+++ AsyncExperiments.Threaded/Program.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Net;
+using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace AsyncExperiments.Threaded
+namespace AsyncExperiments.TapAndEapSamples
 {
     /// <summary>
     /// DISCLAIMER: Some of this code is taken from Pluralsight course, TPL Async Module 1.
@@ -11,43 +12,72 @@ namespace AsyncExperiments.Threaded
     /// </summary>
     class Program
     {
+        [DllImport("kernel32.dll", ExactSpelling = true)]
+
+        private static extern IntPtr GetConsoleWindow();
+        private static readonly IntPtr ThisConsole = GetConsoleWindow();
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        private const int HIDE = 0;
+        private const int MAXIMIZE = 3;
+        private const int MINIMIZE = 6;
+        private const int RESTORE = 9;
+
         static void Main(string[] args)
         {
-            // doesn't work
+            ShowWindow(ThisConsole, MAXIMIZE);
+
+            // works with conditions
             TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;
 
-            Console.WriteLine("Select an operation");
-            Console.WriteLine("1. MultiThreadedTest - Uses Task.Factory.StartNew and continuation with success");
-            Console.WriteLine("2. AsyncTest - Same operation but this time it uses DownloadStringTaskAsync and continuation with success");
-            Console.WriteLine("3. EapTest - Same operation with Event-based Asynchronous Pattern");
-            Console.WriteLine("4. AsyncTestWithFaults - Some error handling experiments");
-            Console.WriteLine("5. AsyncTestWaitWithFault - Some error handling experiments take two, includes one scenario (commented) which will crash the process");
-            Console.WriteLine("6. ContinuationsTest - Continuation scenarios");
+            Console.WriteLine("SELECT AN OPERATION");
+            Console.WriteLine("");
+            Console.WriteLine("1. UsingTaskFactory - Uses Task.Factory.StartNew and continuation with success");
+            Console.WriteLine("2. UsingTaskDirectly - Same operation but this time it uses DownloadStringTaskAsync and continuation with success");
+            Console.WriteLine("3. UsingEventBasedAsynchronousPattern - Same operation with Event-based Asynchronous Pattern");
+            Console.WriteLine("4. UsingTaskDirectlySimulatingFaults - Some error handling experiments");
+            Console.WriteLine("5. UsingTaskDirectlySimulatingFaults_NotCrashingProcessVariant - Simulating faults without crashing the process");
+            Console.WriteLine("6. UsingTaskDirectlySimulatingFaults_CrashingProcessVariant - Simulating faults with crashing the process");
+            Console.WriteLine("7. UsingTaskDirectlySimulatingFaults_AbandoningTaskException - In this scenario, task exception is abandoned. If you follow given instructions in the code this unobserved exception will be handled by UnobservedTaskException event");
+            Console.WriteLine("8. UsingContinuations_OnlyOnSuccess - Continuation scenario, works only antecedent task is successful");
+            Console.WriteLine("9. UsingContinuations_OnlyOnFault - Continuation scenario, works only antecedent task is faulted");
+            Console.WriteLine("");
 
             var result = Console.ReadLine();
 
             switch (result)
             {
                 case "1":
-                    MultiThreadedTest();
+                    UsingTaskFactory();
                     break;
                 case "2":
-                    AsyncTest();
+                    UsingTaskDirectly();
                     break;
                 case "3":
-                    EapTest();
+                    UsingEventBasedAsynchronousPattern();
                     break;
                 case "4":
-                    AsyncTestWithFaults();
+                    UsingTaskDirectlySimulatingFaults();
                     break;
                 case "5":
-                    AsyncTestWaitWithFault();
+                    UsingTaskDirectlySimulatingFaults_NotCrashingProcessVariant();
                     break;
                 case "6":
-                    ContinuationsTest();
+                    UsingTaskDirectlySimulatingFaults_CrashingProcessVariant();
+                    break;
+                case "7":
+                    UsingTaskDirectlySimulatingFaults_AbandoningTaskException();
+                    break;
+                case "8":
+                    UsingContinuations_OnlyOnSuccess();
+                    break;
+                case "9":
+                    UsingContinuations_OnlyOnFault();
                     break;
                 default:
-                    MultiThreadedTest();
+                    UsingTaskFactory();
                     break;
             }
 
@@ -61,7 +91,7 @@ namespace AsyncExperiments.Threaded
             e.SetObserved(); // stop the escalation
         }
 
-        private static void MultiThreadedTest()
+        private static void UsingTaskFactory()
         {
             var webClient = new WebClient();
             Console.WriteLine("Starting work");
@@ -84,7 +114,7 @@ namespace AsyncExperiments.Threaded
             Console.WriteLine("Continuing on main thread");
         }
 
-        private static void AsyncTest()
+        private static void UsingTaskDirectly()
         {
             var webClient = new WebClient();
             Console.WriteLine("Starting work");
@@ -101,40 +131,11 @@ namespace AsyncExperiments.Threaded
             Console.WriteLine("Continuing on main thread");
         }
 
-        private static void AsyncTestWaitWithFault()
-        {
-            var webClient = new WebClient();
-            Console.WriteLine("Starting work");
-
-            //todo:Murat Refactor this scenario into another method
-            webClient.OpenWriteTaskAsync(new Uri("https://localhost:20202")).ContinueWith(t => {
-                Console.WriteLine("Abandoned task, nothing happens. I was expecting to hit UnobservedTaskException");
-            });
-
-            // this doesn't crash the process
-            //webClient.OpenWriteTaskAsync(new Uri("https://localhost:20202")).ContinueWith(t => {
-            //    Console.WriteLine(t.Result);
-            //});
-
-            // but this does
-            // webClient.OpenWriteTaskAsync(new Uri("https://localhost:20202")).Wait();
-
-            Console.WriteLine("If we use Wait, process will crash");
-            Console.ReadLine();
-
-            // For UnobservedTaskException to work:
-            // 1. TPL depends on the Garbage Collector's finalization mechanism to detect unobserved exceptions. This code doesn't do anything so unless we force one
-            // we'll never going to see garbage collection (GC.Collect)
-            // 2. Program needs to be built in Release mode
-            // 3. Start without debugging (Ctrl+F5)
-            GC.Collect();
-        }
-
         /// <summary>
         /// Previous asynchronous model before TPL, before that there was APM but it's ancient coming from .NET 1.0
         /// EAP is .NET >= 2.0 and TPL is .NET >= 4.0
         /// </summary>
-        private static void EapTest()
+        private static void UsingEventBasedAsynchronousPattern()
         {
             var webClient = new WebClient();
             Console.WriteLine("Starting work");
@@ -151,7 +152,7 @@ namespace AsyncExperiments.Threaded
             Console.WriteLine(e.Result);
         }
 
-        private static void AsyncTestWithFaults()
+        private static void UsingTaskDirectlySimulatingFaults()
         {
             var webClient = new WebClient();
             Console.WriteLine("Starting work");
@@ -202,7 +203,56 @@ namespace AsyncExperiments.Threaded
             Console.WriteLine("Continuing on main thread");
         }
 
-        private static void ContinuationsTest()
+        private static void UsingTaskDirectlySimulatingFaults_NotCrashingProcessVariant()
+        {
+            var webClient = new WebClient();
+            Console.WriteLine("Starting work");
+
+            // this doesn't crash the process
+            webClient.OpenWriteTaskAsync(new Uri("https://localhost:20202")).ContinueWith(t =>
+            {
+                Console.WriteLine(t.Result);
+            });
+
+            Console.WriteLine("If we use Wait, process will crash");
+            Console.ReadLine();
+        }
+
+        private static void UsingTaskDirectlySimulatingFaults_CrashingProcessVariant()
+        {
+            var webClient = new WebClient();
+            Console.WriteLine("Starting work");
+
+            // but this does
+            webClient.OpenWriteTaskAsync(new Uri("https://localhost:20202")).Wait();
+
+            Console.WriteLine("If we use Wait, process will crash");
+            Console.ReadLine();
+        }
+
+        // For UnobservedTaskException to work:
+        // 1. TPL depends on the Garbage Collector's finalization mechanism to detect unobserved exceptions. This code doesn't do anything so unless we force one
+        // we'll never going to see garbage collection (GC.Collect)
+        // 2. Program needs to be built in Release mode
+        // 3. Start without debugging (Ctrl+F5)
+        private static void UsingTaskDirectlySimulatingFaults_AbandoningTaskException()
+        {
+            var webClient = new WebClient();
+            Console.WriteLine("Starting work");
+
+            webClient.OpenWriteTaskAsync(new Uri("https://localhost:20202")).ContinueWith(t => {
+                Console.WriteLine("This task will throw an exception but since there's no call to Result or Wait, exception is unobserved");
+                Console.WriteLine("If you follow given instructions, this unobserved exception will be handled by TaskScheduler's UnobservedTaskException event");
+                Console.WriteLine("If you don't, nothing will happen");
+            });
+
+            Console.WriteLine("Continuing on main thread");
+            Console.ReadLine();
+
+            GC.Collect();
+        }
+
+        private static void UsingContinuations_OnlyOnSuccess()
         {
             var webClient = new WebClient();
             Console.WriteLine("Starting work");
@@ -217,14 +267,22 @@ namespace AsyncExperiments.Threaded
                 Console.WriteLine(t.Result);
             }, TaskContinuationOptions.OnlyOnRanToCompletion);
 
-            //Task<string> getTask2 = webClient.DownloadStringTaskAsync("https://localhost:20202");
+            Console.WriteLine("Continuing on main thread");
+        }
 
-            //Console.WriteLine("Setting up continuation");
-            //getTask2.ContinueWith(t =>
-            //{
-            //    // only run if task is faulted
-            //    Console.WriteLine(t.Exception);
-            //}, TaskContinuationOptions.OnlyOnFaulted);
+        private static void UsingContinuations_OnlyOnFault()
+        {
+            var webClient = new WebClient();
+            Console.WriteLine("Starting work");
+
+            Task<string> getTask = webClient.DownloadStringTaskAsync("https://localhost:20202");
+
+            Console.WriteLine("Setting up continuation");
+            getTask.ContinueWith(t =>
+            {
+                // only run if task is faulted
+                Console.WriteLine(t.Exception);
+            }, TaskContinuationOptions.OnlyOnFaulted);
 
             Console.WriteLine("Continuing on main thread");
         }
